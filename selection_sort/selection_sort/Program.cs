41d6378a56dc52937cf796511d5c97c4fc8d4d1d
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-02-03 19:24:03 +0530

    Added selection sort

---
+++ selection_sort/selection_sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace selection_sort
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minimum = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] <= arr[minimum])
+                    {
+                        minimum = j;
+                    }
+                }
+
+                if (minimum != i)
+                {
+                    Swap(ref arr[i], ref arr[minimum]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 8, 5, 3, 9, 4, 1 };
+            SelectionSort(arr);
+        }
+    }
+}
