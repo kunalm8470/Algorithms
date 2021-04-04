efb51f7b8d19a642e9c3835e12bc81076519ceee
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-04 23:03:59 +0530

    Added stack implementation

---
+++ stack/stack/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stk = new Stack();
+            stk.Push(1);
+            stk.Push(2);
+            stk.Push(3);
+            stk.Push(4);
+            stk.Push(5);
+
+            int popped = stk.Pop();
+        }
+    }
+}
---
+++ stack/stack/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace stack
+{
+    public class Stack
+    {
+        private int[] _values;
+        private int _count = 0;
+        private static readonly int DEFAULT_SIZE = 4;
+
+        // Constructors
+        public Stack(int capacity)
+        {
+            _values = new int[capacity];
+            _count = 0;
+        }
+
+        public Stack() : this(DEFAULT_SIZE) { }
+        
+        public int Count 
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public int Peek()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Empty stack!");
+            }
+
+            return _values[_count - 1];
+        }
+
+        public void Push(int item)
+        {
+            if (_count == _values.Length)
+            {
+                Array.Resize(ref _values, 2 * _values.Length);
+            }
+                
+            _values[_count++] = item;
+        }
+
+        public int Pop()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Empty stack!");
+            }
+
+            int result = _values[--_count];
+            _values[_count] = default(int);
+            return result;
+        }
+    }
+}
