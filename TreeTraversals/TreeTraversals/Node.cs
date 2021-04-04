f38f5294781273c2176e67d15eaa66d87cab1883
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-04 21:39:19 +0530

    Added tree traversals

---
+++ TreeTraversals/TreeTraversals/Node.cs
@@ -0,0 +1,17 @@
+﻿namespace TreeTraversals
+{
+    /// <summary>
+    /// POCO to represent each node in the BST
+    /// </summary>
+    public class Node
+    {
+        public int Item { get; set; }
+        public Node Left { get; set; } = default(Node);
+        public Node Right { get; set; } = default(Node);
+
+        public Node(int item)
+        {
+            this.Item = item;
+        }
+    }
+}
