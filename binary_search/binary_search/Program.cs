05f6f8e6b61fdd6d94b26893c88c7b8b2b820be6
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-04 20:47:54 +0530

    Added binary search

---
+++ binary_search/binary_search/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace binary_search
+{
+    class Program
+    {
+        public static int BinarySearch(int[] arr, int item)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (item > arr[left])
+                {
+                    left++;
+                }
+                else if (item < arr[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            throw new ArgumentException($"{nameof(item)} not found", nameof(item));
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 7, 12, 13, 16, 17, 22, 24 };
+            int item = 17;
+            int idx = BinarySearch(arr, item);
+        }
+    }
+}
