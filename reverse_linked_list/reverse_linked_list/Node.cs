53cfec7cb7ab8babab6e3ac7a442916deb27ba43
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-10 21:11:34 +0530

    Added reversal of linked list

---
+++ reverse_linked_list/reverse_linked_list/Node.cs
@@ -0,0 +1,33 @@
+﻿namespace reverse_linked_list
+{
+    public class Node
+    {
+        public int Data { get; }
+        public Node Next { get; }
+
+        public Node(int data, Node next)
+        {
+            Data = data;
+            Next = next;
+        }
+
+        public static Node Reverse(Node head)
+        {
+            if (head == default) return default;
+
+            if (head.Next == default) return head;
+
+            Node current = head;
+            Node reverse = default;
+
+            while (current.Next != default)
+            {
+                reverse = new Node(current.Data, reverse);
+                current = current.Next;
+            }
+
+            reverse = new Node(current.Data, reverse);
+            return reverse;
+        }
+    }
+}
---
+++ reverse_linked_list/reverse_linked_list/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace reverse_linked_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node head = default;
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                head = new Node(arr[i], head);
+            }
+
+            Node reversed = Node.Reverse(head);
+        }
+    }
+}
