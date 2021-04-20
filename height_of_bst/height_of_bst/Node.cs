2f943a961a643677adb9a73452343a52e7909d56
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-20 22:16:59 +0530

    Added calculating height of BST

---
+++ height_of_bst/height_of_bst/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace height_of_bst
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public static Node GenerateBST(int[] nums, int left, int right)
+        {
+            if (left > right)
+            {
+                return default;
+            }
+
+            int mid = left + (right - left) / 2;
+            Node root = new Node();
+            root.Data = nums[mid];
+            root.Left = GenerateBST(nums, left, mid - 1);
+            root.Right = GenerateBST(nums, mid + 1, right);
+            return root;
+        }
+
+        public static int CalculateBSTHeight(Node root)
+        {
+            if (root == default) return 0;
+
+            int leftSubTreeHeight = CalculateBSTHeight(root.Left);
+            int rightSubTreeHeight = CalculateBSTHeight(root.Right);
+
+            return Math.Max(leftSubTreeHeight, rightSubTreeHeight) + 1;
+        }
+    }
+}
