f4846ab5441038d4c1236aed05246dc2d1e7a7de
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-20 20:07:45 +0530

    Added printing of leaf nodes

---
+++ printing_leaf_nodes/printing_leaf_nodes/Models/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace printing_leaf_nodes.Models
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public static Node GenerateBST(int[] arr, int left, int right)
+        {
+            if (left > right) return default;
+            
+            int mid = left + (right - left) / 2;
+            Node root = new Node
+            {
+                Data = arr[mid]
+            };
+            
+            root.Left = GenerateBST(arr, left, mid - 1);
+            root.Right = GenerateBST(arr, mid + 1, right);
+            return root;
+        }
+
+        public static void PrintLeafNodes(Node root)
+        {
+            if (root == default) return;
+
+            /*
+             * Leaf nodes have left and right nodes as null
+            */
+            if (root.Left == default && root.Right == default)
+            {
+                Console.Write("{0} ", root.Data);
+            }
+
+            // Recurse into left subtree
+            PrintLeafNodes(root.Left);
+
+            // Recurse into right subtree
+            PrintLeafNodes(root.Right);
+        }
+    }
+}
---
+++ printing_leaf_nodes/printing_leaf_nodes/Program.cs
@@ -0,0 +1,16 @@
+﻿using printing_leaf_nodes.Models;
+using System;
+
+namespace printing_leaf_nodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 8, 10, 12, 15, 16, 20, 25 };
+            Node bstRoot = Node.GenerateBST(arr, 0, arr.Length - 1);
+
+            Node.PrintLeafNodes(bstRoot);
+        }
+    }
+}
