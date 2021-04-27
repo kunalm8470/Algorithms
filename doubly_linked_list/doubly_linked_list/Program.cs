9f64489efdd178a11e6b3ec8d1699ebc26f07aa5
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-27 23:10:31 +0530

    Added implementation of double linked list

---
+++ doubly_linked_list/doubly_linked_list/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace doubly_linked_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList<int> ll = new DoubleLinkedList<int>();
+            ll.AddLast(1).AddLast(2).AddLast(3).AddLast(4).AddLast(5).AddLast(6);
+
+            ll.Remove(3).Remove(5);
+            foreach (Node<int> item in ll)
+            {
+                Console.WriteLine(item.Data);
+            }
+
+            ll.RemoveFirst();
+            foreach (Node<int> item in ll)
+            {
+                Console.WriteLine(item.Data);
+            }
+        }
+    }
+}
