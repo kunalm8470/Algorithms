3e059bccb0883b7893f9038410877c13dcb4b456
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-04-28 00:37:49 +0530

    Added deletion of node from single linked list

---
+++ deleting_occurences_linked_list/deleting_occurences_linked_list/Node.cs
@@ -0,0 +1,8 @@
+﻿namespace deleting_occurences_linked_list
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ deleting_occurences_linked_list/deleting_occurences_linked_list/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace deleting_occurences_linked_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList<int> sl = new SingleLinkedList<int>();
+            sl.AddLast(1).AddLast(2).AddLast(3).AddLast(4).AddLast(5);
+            
+            sl.Remove(2);
+
+            foreach (Node<int> item in sl)
+            {
+                Console.WriteLine(item.Data);
+            }
+            
+        }
+    }
+}
