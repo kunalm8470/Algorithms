c1c4d4c8a21b0dfe7067929e1ccd5f04291b6a77
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-05-27 10:58:49 +0530

    Update quick sort implementation to generic

--- quick_sort/quick_sort/Program.cs
+++ quick_sort/quick_sort/Program.cs
@@ -1,85 +1,11 @@
-﻿using System;
-
-namespace quick_sort
+﻿namespace quick_sort
 {
     class Program
     {
-        static void QuickSort(int[] arr)
-        {
-            quickSort(arr, 0, arr.Length - 1);
-        }
-
-        static void quickSort(int[] arr, int left, int right)
-        {
-            if (left >= right)
-            {
-                return;
-            }
-
-            int partitionIndex = Partition(arr, left, right);
-            quickSort(arr, left, partitionIndex);
-            quickSort(arr, partitionIndex + 1, right);
-        }
-
-        /// <summary>
-        /// Hoare's partition algorithm
-        /// </summary>
-        /// <param name="arr">Array</param>
-        /// <param name="left">Left index</param>
-        /// <param name="right">Right index</param>
-        /// <returns>Partition index (i.e index of the pivot element which is in correct sorted order)</returns>
-        static int Partition(int[] arr, int left, int right)
-        {
-            int pivot = arr[left];
-            int i = left - 1;
-            int j = right + 1;
-
-            while (true)
-            {
-                // Increment until found a number larger than pivot
-                do
-                {
-                    i += 1;
-                } while (arr[i] < pivot);
-
-                // Decrement until found a number smaller than pivot
-                do
-                {
-                    j -= 1;
-                } while (arr[j] > pivot);
-
-                /*
-                 * At this point left has crossed right, break out
-                 * of the loop
-                 * 
-                 * Pivot will be now at the correct sorted position
-                 * and elements to the left of pivot will be smaller
-                 * and to the right will be greater
-                */
-                if (i >= j)
-                {
-                    return j;
-                }
-
-                /*
-                 * Swap the first-or-default value larger than the pivot
-                 * with the first-or-default value smaller than the pivot
-                */
-                Swap(ref arr[i], ref arr[j]);
-            }
-        }
-
-        static void Swap(ref int a, ref int b)
-        {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-
         static void Main(string[] args)
         {
             int[] arr = { -1, -2, -1, -3, -3, 14, 6, 8, 1, 4, 9, 2, 0, 1, 7, 10, 5 };
-            QuickSort(arr);
+            arr.QuickSort();
         }
     }
 }
---
+++ quick_sort/quick_sort/QuickSortExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace quick_sort
+{
+    public static class QuickSortExtensions
+    {
+        public static void QuickSort<T>(this T[] arr) where T : IComparable
+        {
+            quickSort(arr, 0, arr.Length - 1);
+        }
+
+        static void quickSort<T>(T[] arr, int left, int right) where T : IComparable
+        {
+            if (left >= right) return;
+
+            int partitionIndex = Partition(arr, left, right);
+            quickSort(arr, left, partitionIndex);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// Hoare's partition algorithm
+        /// </summary>
+        /// <param name="arr">Array</param>
+        /// <param name="left">Left index</param>
+        /// <param name="right">Right index</param>
+        /// <returns>Partition index (i.e index of the pivot element which is in correct sorted order)</returns>
+        static int Partition<T>(T[] arr, int left, int right) where T : IComparable
+        {
+            T pivot = arr[left];
+            int i = left - 1;
+            int j = right + 1;
+
+            while (true)
+            {
+                // Increment until found a number larger than pivot
+                do
+                {
+                    i += 1;
+                } while (arr[i].CompareTo(pivot) < 0);
+
+                // Decrement until found a number smaller than pivot
+                do
+                {
+                    j -= 1;
+                } while (arr[j].CompareTo(pivot) > 0);
+
+                /*
+                 * At this point left has crossed right, break out
+                 * of the loop
+                 * 
+                 * Pivot will be now at the correct sorted position
+                 * and elements to the left of pivot will be smaller
+                 * and to the right will be greater
+                */
+                if (i >= j) return j;
+
+                /*
+                 * Swap the first-or-default value larger than the pivot
+                 * with the first-or-default value smaller than the pivot
+                */
+                Swap(ref arr[i], ref arr[j]);
+            }
+        }
+    }
+}
