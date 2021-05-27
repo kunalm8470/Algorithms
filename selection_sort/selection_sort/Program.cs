3c194de8c1c49c11039e498e03a85198d3c0a4c1
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-05-27 11:16:36 +0530

    Updated selection sort implementation to generic

--- selection_sort/selection_sort/Program.cs
+++ selection_sort/selection_sort/Program.cs
@@ -1,40 +1,11 @@
-﻿using System;
-
-namespace selection_sort
+﻿namespace selection_sort
 {
     class Program
     {
-        static void Swap(ref int a, ref int b)
-        {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-
-        static void SelectionSort(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                int minimum = i;
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[j] < arr[minimum])
-                    {
-                        minimum = j;
-                    }
-                }
-
-                if (minimum != i)
-                {
-                    Swap(ref arr[i], ref arr[minimum]);
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            int[] arr = { 2, 8, 5, 3, 9, 4, 1 };
-            SelectionSort(arr);
+            int[] arr = { 2, 8, -2, -1, -7, 0, 5, 3, 9, 4, 1 };
+            arr.SelectionSort();
         }
     }
 }
---
+++ selection_sort/selection_sort/SelectionSortExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace selection_sort
+{
+    public static class SelectionSortExtensions
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void SelectionSort<T>(this T[] arr) where T : IComparable
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minimum = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j].CompareTo(arr[minimum]) < 0)
+                        minimum = j;
+                }
+
+                if (minimum != i)
+                    Swap(ref arr[i], ref arr[minimum]);
+            }
+        }
+    }
+}
