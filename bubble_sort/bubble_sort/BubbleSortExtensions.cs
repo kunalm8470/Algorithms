a78fc644578c26e751a9ab6263ee8b8d156edaac
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-05-27 11:10:40 +0530

    Updated bubble sort implementation to generic

---
+++ bubble_sort/bubble_sort/BubbleSortExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace bubble_sort
+{
+    public static class BubbleSortExtensions
+    {
+        public static void BubbleSort<T>(this T[] arr) where T : IComparable
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (arr[i].CompareTo(arr[j]) > 0)
+                        Swap(ref arr[i], ref arr[j]);
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
